609f20660df1420eb2ed021a423fcb76c5fcb45d
norbert <norbert.corba@gmail.com>
2022-11-15 20:58:45 +0100

    added Cars model and seeders

---
+++ database/seeders/CarSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Cars;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CarSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Cars::factory(10)->create();
+
+    }
+}
