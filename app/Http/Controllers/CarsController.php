234f216803285278653e0505e067e0b0056ce015
norbert <norbert.corba@gmail.com>
2022-11-22 20:47:54 +0100

    starting jwt

---
+++ app/Http/Controllers/CarsController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateCarRequest;
+use App\Models\Cars;
+use Illuminate\Http\Request;
+
+class CarsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {   
+        $perPage = $request->query('per_page', 5);
+        $brandTerm = $request->query('brand');
+        $model = $request->query('model');
+
+        return Cars::query()->searchByBrand($brandTerm)->searchByModel($model)->get();
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateCarRequest $request)
+    {
+        $validated = $request->validated();
+
+        $cars = new Cars();
+        $cars->brand = $validated['brand'];
+        $cars->model = $validated['model'];
+        $cars->year = $validated['year'];
+        $cars->max_speed = $validated['max_speed'];
+        $cars->is_automatic = $validated['is_automatic'];
+        $cars->engine = $validated['engine'];
+        $cars->number_of_doors = $validated['number_of_doors'];
+        $cars->save();
+        return $cars;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $cars = Cars::find($id);
+        return $cars;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(CreateCarRequest $request, $id)
+    {
+        $validated = $request->validated();
+
+        $car = Cars::find($id);
+        $car->brand = $validated['brand'];
+        $car->model = $validated['model'];
+        $car->year = $validated['year'];
+        $car->max_speed = $validated['max_speed'];
+        $car->is_automatic = $validated['is_automatic'];
+        $car->engine = $validated['engine'];
+        $car->number_of_doors = $validated['number_of_doors'];
+        $car->save();
+        return $car;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $car = Cars::find($id)->delete();
+        return $car;
+    }
+}
--- app/Models/Cars.php
+++ app/Models/Cars.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Cars extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'brand',
+        'model',
+        'year',
+        'max_speed',
+        'is_automatic',
+        'engine',
+        'number_of_doors',
+    ];
+
+    public function scopeSearchByBrand($query, $term)
+    {
+       return $query->where('brand','LIKE','%'.$term.'%');
+    }
+
+    public function scopeSearchByModel($query, $model)
+    {
+       return $query->where('model','LIKE','%'.$model.'%');
+    }
+
 }
